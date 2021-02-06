34f6a1183b37fb4738788e237fb1ece7cea15a8c
yoonho <yoonho0922@naver.com>
2021-02-06 20:21:50 +0900

    refactor : 에러 핸들러로 404 에러 처리'

--- app.js
+++ app.js
@@ -1,13 +1,12 @@
-//==== 모듈 불러오기 ====//
 // 기본 모듈 불러오기
 const express = require('express')
     , http = require('http')
     , path = require('path');
 
-// 외장 모듈 불러오기
+// express 미들웨어 불러오기
 const static = require('serve-static')
-    , bodyParser = require('body-parser');
-
+    , bodyParser = require('body-parser')
+    , expressErrorHandler = require('express-error-handler');
 
 //==== 익스프레스 객체 생성 ====//
 const app = express();
@@ -32,22 +31,24 @@ app.use(bodyParser.json())
 //==== 데이터베이스 관련 ====//
 const database = require('./database/database');
 
+database.init(app, config);
 
 //==== 라우팅 관련 ====//
 const route_loader = require('./routes/route_loader');
 
 route_loader.init(app, express.Router());
 
-// 404 에러 처리
-app.all('*', function(req, res){
-    res.status(404).send('<p>ERROR - 페이지를 찾을 수 없습니다.</p>');
-})
+//==== 에러 핸들러 ====//
+const errorHandler = expressErrorHandler({
+    static: {
+        '404': './public/404.html'
+    }
+});
 
+app.use(expressErrorHandler.httpError(404));
+app.use(errorHandler);
 
 //==== 서버 시작 ====//
 http.createServer(app).listen(app.get('port'), function(){
     console.log('서버가 시작되었습니다. 포트 : ', + app.get('port'));
-
-    // 데이터베이스 초기화
-    database.init(app, config);
 });
